0cae8ccec40bd5cc0c09179c38470e9dfb1a29e8
Monster205 <3180454103@qq.com>
2025-03-13 02:18:34 +0800

    test
    test

---
+++ omni_PDSM00.mk
@@ -0,0 +1,63 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# Configure twrp common.mk
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Configure base.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+PRODUCT_DEVICE := PDSM00
+PRODUCT_NAME := omni_PDSM00
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := OPPO Reno5 Pro
+PRODUCT_MANUFACTURER := OPPO
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Health
+#PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Drm
+#PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4
+
+# Keystore
+#PRODUCT_PACKAGES += \
+    android.system.keystore2
+
+# Keymaster
+#PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1
+
+# HACK: Set vendor patch level
+#PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31 
